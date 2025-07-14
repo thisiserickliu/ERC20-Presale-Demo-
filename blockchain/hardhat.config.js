f6290a336645eee35caf48f01a2f64a3b2ad7cf0
thisiserickliu <policeplease06@gmail.com>
2025-07-14 14:47:47 +0800

    Sepolia Test

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -20,11 +20,7 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     sepolia: {
-      url: process.env.SEPOLIA_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-    mainnet: {
-      url: process.env.MAINNET_URL || "",
+      url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.20",
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
     sepolia: {
-      url: process.env.SEPOLIA_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-    mainnet: {
-      url: process.env.MAINNET_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+      url: "https://eth-sepolia.g.alchemy.com/v2/vTRr0ICPjUuFOwqBkrmKA",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 }; 
